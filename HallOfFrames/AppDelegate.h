2303998e0070c3f8019287ae35b9be07fa34b414
Yemi Ajibola <ya9p8@mst.edu>
2016-01-20 13:49:48 -0600

    Initial Commit

---
+++ HallOfFrames/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HallOfFrames
+//
+//  Created by Yemi Ajibola on 1/20/16.
+//  Copyright © 2016 Yemi Ajibola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HallOfFrames/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HallOfFrames
+//
+//  Created by Yemi Ajibola on 1/20/16.
+//  Copyright © 2016 Yemi Ajibola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
